301f4d0f6b21fcad4a7cce769fa61ec4f1a8b703
ZeDiB <zeynepdilara.b@gmail.com>
2021-12-22 22:45:49 +0000

    added change

--- src/test/java/Testin.java
+++ src/test/java/Testin.java
@@ -2,5 +2,6 @@ public class Testin {
     public static void main(String[] args) {
         System.out.println("Hello intellij");
         System.out.println("Add more work");
+        System.out.println("orhan zeynep i cok seviyor");
     }
 }
